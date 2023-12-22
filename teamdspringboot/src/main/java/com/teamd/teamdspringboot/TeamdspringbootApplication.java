3833f873bfb4ed5ebfc92724fec4a2d29106fa89
jhakes05 <jake.atillo.b@gmail.edu.ph>
2023-12-22 15:23:37 +0800

    added springboot installation

---
+++ teamdspringboot/src/main/java/com/teamd/teamdspringboot/TeamdspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.teamd.teamdspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamdspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamdspringbootApplication.class, args);
+	}
+
+}
---
+++ teamdspringboot/src/test/java/com/teamd/teamdspringboot/TeamdspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamd.teamdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamdspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
